055faa4c3995d79068a9fe502b36f19c3438c935
Muhammad Bin Jamil <binjamil35@gmail.com>
2019-05-03 04:31:17 +0500

    added css styling

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <ul class="grid">
       <movie-list-item
         v-for="movie in movies"
         :movie="movie"
@@ -27,4 +27,18 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  grid-column-gap: 6rem;
+  grid-row-gap: 3rem;
+  padding: 0;
+  margin-top: 4rem;
+
+  @media screen and (max-width: 600px) {
+    grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+    margin-top: 2rem;
+  }
+}
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <input v-model="searchQuery" @keyup.enter="search" />
-    <span v-if="error">{{ error }}</span>
+  <div class="search">
+    <input
+      class="search__input"
+      v-model="searchQuery"
+      @keyup.enter="search"
+      placeholder="Search your movies..."
+    />
+    <div class="error" v-if="error">
+      <i class="fas fa-exclamation-circle error__icon"></i>
+      <span class="error__text">{{ error }}</span>
+    </div>
   </div>
 </template>
 
@@ -35,3 +43,45 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.search {
+  &__input {
+    font-size: 5rem;
+    font-family: inherit;
+    font-weight: 700;
+    background-color: transparent;
+    border: none;
+    color: var(--white);
+    border-left: 0.5rem solid var(--blue-light);
+    padding: 1.5rem;
+
+    &:focus {
+      outline: none;
+    }
+
+    @media screen and (max-width: 900px) {
+      font-size: 4rem;
+    }
+
+    @media screen and (max-width: 600px) {
+      font-size: 3rem;
+      padding: 1.2rem;
+    }
+  }
+}
+
+.error {
+  margin-top: 1.5rem;
+  font-size: 2rem;
+
+  &__icon {
+    color: var(--yellow);
+  }
+
+  &__text {
+    color: var(--white);
+    margin-left: 1rem;
+  }
+}
+</style>
