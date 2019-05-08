0029792cd22ac607d5897bf64ba357507c80c5f7
Muhammad Bin Jamil <binjamil35@gmail.com>
2019-05-08 22:01:46 +0500

    fixed css grid logical error

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -30,14 +30,14 @@ export default {
 <style lang="scss" scoped>
 .grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  grid-template-columns: repeat(auto-fit, 30rem);
   grid-column-gap: 6rem;
   grid-row-gap: 3rem;
   padding: 0;
   margin-top: 4rem;
 
   @media screen and (max-width: 600px) {
-    grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+    grid-template-columns: repeat(auto-fill, 30rem);
     margin-top: 2rem;
   }
 }
